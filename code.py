109753602cef8369482575a77773ed500e969fcc
donaldhumphrey <142233347+donaldhumphrey@users.noreply.github.com>
2023-11-01 08:59:15 -0400

    Add files via upload

---
+++ code.py
@@ -0,0 +1,48 @@
+# Simple code to read a message from the Dabble App over the DSDTech HM-10 bluetooth module
+# Author: Eric Z. Ayers <ericzundel@gmail.com>
+
+"""CircuitPython Example of how to read data from the Dabble app"""
+import binascii #import libraries that are needed to run the code
+import board
+import busio
+import digitalio
+import time
+
+from dabble import Dabble
+
+dabble = Dabble(board.GP0, board.GP1, debug=True) #defines hardware and attaches it to a pin
+
+while True:
+    message = dabble.read_message()
+    if (message != None):
+        print("Message: " + str(message))
+        # Implement tank steering on a 2 wheeled robot
+        if (message.up_arrow_pressed):
+            print("Move both motors forward")
+        elif (message.down_arrow_pressed):
+            print("Move both motors backward")
+        elif (message.right_arrow_pressed):
+            print("Move left motor forward and right motor backward")
+        elif (message.left_arrow_pressed):
+            print("Move left motor backward and right motor forward")
+        elif (message.no_direction_pressed):
+            print("Stop both motors")
+        else:
+            print("Something crazy happened with direction!")
+
+        if (message.triangle_pressed):
+            print("Raise arm")
+        elif (message.circle_pressed):
+            print("Lower arm")
+        elif (message.square_pressed):
+            print("Squirt water")
+        elif (message.circle_pressed):
+            print("Fire laser")
+        elif (message.start_pressed):
+            print("Turn on LED")
+        elif (message.select_pressed):
+            print("Do victory dance")
+        elif (message.no_action_pressed):
+            print("No action")
+        else:
+            print("Something crazy happened with action!")
